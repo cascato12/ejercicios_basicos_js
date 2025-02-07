31500d997087453706c48e9663823b0f1cc0d2e8
Arturo Salcedo <arturosalcedo508@gmail.com>
2025-02-07 12:48:39 +0100

    Ejercicio 7 Hecho

--- Ejercicio7.js
+++ Ejercicio7.js
@@ -1,5 +1,12 @@
-Completa esta función para que, al recibir dos números por argumento, te devuelva por consola el más alto de los dos.
+/*Completa esta función para que, al recibir dos números por argumento, te devuelva por consola el más alto de los dos.*/
 
-function greaterNumber(numberOne , numberTwo) {
-  // Completar código
+function greaterNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne)
+  } else {
+    console.log(numberTwo)
+  }
 }
+//Ejemplo
+greaterNumber(10, 20)
+greaterNumber(50, 15)
