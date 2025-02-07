5824f7b04b1af045959295aaa3b0e3cdfd9f4cbb
Arturo Salcedo <arturosalcedo508@gmail.com>
2025-02-07 13:15:06 +0100

    Ejercicios 9 y 10 hechos

--- Ejercicio10.js
+++ Ejercicio10.js
@@ -1,8 +1,15 @@
-Calcular un promedio es una tarea extremadamente común, así que prueba a implementar esa funcionalidad en la siguiente función.
+/*Calcular un promedio es una tarea extremadamente común, así que prueba a implementar esa funcionalidad en la siguiente función.
 
-Puedes usar este array para probarla:
+Puedes usar este array para probarla:*/
 
-const numbers = [12, 21, 38, 5, 45, 37, 6];
+const numbers = [12, 21, 38, 5, 45, 37, 6]
 function average(numberList) {
-  // Completar
+  let sum = 0
+  for (let i = 0; i < numberList.length; i++) {
+    sum += numberList[i]
+  }
+  const avg = sum / numberList.length
+  return avg
 }
+
+console.log(average(numbers))
--- Ejercicio9.js
+++ Ejercicio9.js
@@ -1,11 +1,19 @@
-Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+/*Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
 
 Completa la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números del array.
 
-Puedes usar este array para probar tu función:
+Puedes usar este array para probar tu función:*/
 
-const numbers = [1, 2, 3, 5, 45, 37, 58];
+const numbers = [1, 2, 3, 5, 45, 37, 58]
 
-function sumNumbers(numberList) {
-  // Completar código
+function sumNumbers(numbers) {
+  let sum = 0
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+  }
+
+  return sum
 }
+
+console.log(sumNumbers(numbers))
